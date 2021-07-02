d329e03ee56fc38dd4c73618f6030b31180c937b
Gershwen <76742941+Gershwen@users.noreply.github.com>
2021-07-02 16:03:50 +0200

    Add files via upload
    3rd commit. Completed desktop and tablet view

--- planets-fact-site/starter-code/index.js
+++ planets-fact-site/starter-code/index.js
@@ -20,6 +20,8 @@ $("button").click(function() {
     $(this).addClass("active");
  });
 
+
+
 // Mercury
 
  var mercuryPara = ["Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.", "Mercury appears to have a solid silicate crust and mantle overlying a solid, iron sulfide outer core layer, a deeper liquid core layer, and a solid inner core. The planet's density is the second highest in the Solar System at 5.427 g/cm3 , only slightly less than Earth's density.", "Mercury's surface is similar in appearance to that of the Moon, showing extensive mare-like plains and heavy cratering, indicating that it has been geologically inactive for billions of years. It is more heterogeneous than either Mars's or the Moon’s."];
